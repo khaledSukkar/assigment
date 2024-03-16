cbb8ced3ba8a255d2200a11537fa49941632b5b3
khaledSukkar <khaledsukkar2@gmail.com>
2024-03-16 02:45:16 +0200

    Finishing the views

--- invoices/views.py
+++ invoices/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Invoice, InvoiceDetail
+from .serializers import InvoiceSerializer, InvoiceDetailSerializer
 
-# Create your views here.
+class InvoiceViewSet(viewsets.ModelViewSet):
+    """
+    This is the ViewSet for Invoice model
+    """
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+
+class InvoiceDetailViewSet(viewsets.ModelViewSet):
+    """
+    This is thte Viewset for Invoice detail model
+    """
+    queryset = InvoiceDetail.objects.all()
+    serializer_class = InvoiceDetailSerializer
